79d7fe0f834f57e23cfb4e87f8fba90d4cd77930
jcbain <jamescbain@gmail.com>
2022-04-12 14:45:26 -0500

    fix name

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-      <Header title="Pat's Pizza" />
+      <Header title="Pat's Pizza!" />
     </div>
   );
 }
